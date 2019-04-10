d60a16fffa20b88a50dff4d2baf264d7d2b65d22
Unknown <millza04@gettysburg.edu>
2019-04-10 19:33:56 -0400

    Basis setup of documentation
    Just a set up of the basic documentation for the code. Next step is to populate the Pseduo code file with a break down of how to implement this.

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# Written by: Zachary Miller
+# Date: 4/12/2019
+# Purpose: Code will take in input file and display output data based on input
+# View "Pseudo.txt" for additional information on implemtation
+
+
+print("Hello World")
